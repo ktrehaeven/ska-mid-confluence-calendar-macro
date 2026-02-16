bb0e0def133713d63a244a8a0f53e257d45e50ef
[Jesse Cooper] <[jesse.cooper@skao.int]>
2026-02-16 14:08:02 +0800

    change resources from general context to only as required by macros

--- src/main/java/com/skao/confluence/plugins/macro/skalowmap.java
+++ src/main/java/com/skao/confluence/plugins/macro/skalowmap.java
@@ -3,14 +3,25 @@ package com.skao.confluence.plugins.macro;
 import com.atlassian.confluence.content.render.xhtml.ConversionContext;
 import com.atlassian.confluence.macro.Macro;
 import com.atlassian.confluence.macro.MacroExecutionException;
+import com.atlassian.plugin.webresource.WebResourceManager;
 
 import java.util.Map;
 
 public class SkaLowMap implements Macro {
 
+    private final WebResourceManager webResourceManager;
+
+    public SkaLowMap(WebResourceManager webResourceManager) {
+        this.webResourceManager = webResourceManager;
+    }
+
     @Override
     public String execute(Map<String, String> params, String body, ConversionContext ctx)
             throws MacroExecutionException {
+
+        // Require the web resources for this macro
+        webResourceManager.requireResource("com.skao.confluence.plugins.ska-low-confluence-calendar-macro:leaflet-resources");
+        webResourceManager.requireResource("com.skao.confluence.plugins.ska-low-confluence-calendar-macro:ska-low-bootstrap-js");
 
         // HTML wrapper for the Leaflet map
         return "<div class='ska-low-map-macro'>" +  
--- src/main/java/com/skao/confluence/plugins/macro/skalowstationbookings.java
+++ src/main/java/com/skao/confluence/plugins/macro/skalowstationbookings.java
@@ -3,12 +3,24 @@ package com.skao.confluence.plugins.macro;
 import com.atlassian.confluence.content.render.xhtml.ConversionContext;
 import com.atlassian.confluence.macro.Macro;
 import com.atlassian.confluence.macro.MacroExecutionException;
+import com.atlassian.plugin.webresource.WebResourceManager;
 
 import java.util.Map;
 
 public class SkaLowStationBookings implements Macro {
 
+    private final WebResourceManager webResourceManager;
+
+    public SkaLowStationBookings(WebResourceManager webResourceManager) {
+        this.webResourceManager = webResourceManager;
+    }
+
     public String execute(Map<String, String> map, String s, ConversionContext conversionContext) throws MacroExecutionException {
+        // Require the web resources for this macro
+        webResourceManager.requireResource("com.skao.confluence.plugins.ska-low-confluence-calendar-macro:daypilot-resources");
+        webResourceManager.requireResource("com.skao.confluence.plugins.ska-low-confluence-calendar-macro:ska-low-confluence-calendar-macro-resources");
+        webResourceManager.requireResource("com.skao.confluence.plugins.ska-low-confluence-calendar-macro:ska-low-bootstrap-js");
+        
         return "<div class='ska-low-station-bookings-macro'>" +  
                 " <div class='daypilot-nav'></div>" +
                 " <div class='daypilot'></div>" +
