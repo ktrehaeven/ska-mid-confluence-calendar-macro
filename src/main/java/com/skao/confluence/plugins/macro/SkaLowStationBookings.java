664db586b28e1ed51bf08a72d29c576739cce7f0
[Jesse Cooper] <[jesse.cooper@skao.int]>
2026-03-11 10:51:35 +0800

    rename calendar id from browser for consistency

--- src/main/java/com/skao/confluence/plugins/macro/SkaLowStationBookings.java
+++ src/main/java/com/skao/confluence/plugins/macro/SkaLowStationBookings.java
@@ -15,15 +15,15 @@ public class SkaLowStationBookings implements Macro {
 
     public String execute(Map<String, String> map, String s, ConversionContext conversionContext) throws MacroExecutionException {
 
-        // parameter passed in from the macro browser, should be a comma separated list of calendar ids
-        String calendarIds = map.get("Calendar");
-        if (calendarIds == null) calendarIds = "";
+        // parameter passed in from the macro browser, should be a comma separated list of sub calendar ids
+        String subCalendarIds = map.get("Calendar");
+        if (subCalendarIds == null) subCalendarIds = "";
 
         webResourceManager.requireResource("com.skao.confluence.plugins.ska-low-confluence-calendar-macro:daypilot-resources");
         webResourceManager.requireResource("com.skao.confluence.plugins.ska-low-confluence-calendar-macro:ska-low-confluence-calendar-macro-resources");
         webResourceManager.requireResource("com.skao.confluence.plugins.ska-low-confluence-calendar-macro:ska-low-bootstrap-js");
 
-        return "<div class='ska-low-station-bookings-macro' data-calendar-ids='" + calendarIds + "'>" +
+        return "<div class='ska-low-station-bookings-macro' data-calendar-ids='" + subCalendarIds + "'>" +
             "<div class='nav-panel'>" +
                 "<div class='daypilot-nav'></div>" +
             "</div>" +
